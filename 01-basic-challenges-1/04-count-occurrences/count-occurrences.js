b6fd7322e6729f043982c0c3047672ccc40ed407
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-09-03 21:24:17 -0600

    9. Count Occurences

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,33 @@
-function countOccurrences() {}
+// *** Solution1 ***
+function countOccurrences(str, char) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = countOccurrences;
+
+// Case insensitive version
+// function countOccurrences(str, char) {
+//   const lowerStr = str.toLowerCase();
+//   const lowerChar = char.toLowerCase();
+
+//   let count = 0;
+
+//   for (let i = 0; i < lowerStr.length; i++) {
+//     if (lowerStr[i] === lowerChar) {
+//       count++;
+//     }
+//   }
+
+//   return count;
+// }
+
+// *** Solution 2 ***
+// const countOccurrences = (str, char) => str.split(char).length - 1;

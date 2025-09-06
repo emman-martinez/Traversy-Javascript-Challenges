37f689147394c6245e31c62fb3d4dff8fa0e8d2e
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-09-05 22:43:44 -0600

    15. Remove Duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,19 @@
-function removeDuplicates() {}
+// *** Solution1 ***
+// function removeDuplicates(arr) {
+//   const uniqueArr = [];
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (!uniqueArr.includes(arr[i])) {
+//       uniqueArr.push(arr[i]);
+//     }
+//   }
+
+//   return uniqueArr;
+// }
+
+// *** Solution2 ***
+function removeDuplicates(arr) {
+  return Array.from(new Set(arr));
+}
 
 module.exports = removeDuplicates;

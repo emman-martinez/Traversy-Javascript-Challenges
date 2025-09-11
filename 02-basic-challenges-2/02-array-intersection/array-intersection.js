c325e3498bf1d19c38cbce5b871202639f4c1f0c
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-09-10 20:19:34 -0600

    17. Array Intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,28 @@
-function arrayIntersection() {}
+// *** Solution 1 ***
+function arrayIntersection(arr1, arr2) {
+  const intersection = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !intersection.includes(arr1[i])) {
+      intersection.push(arr1[i]);
+    }
+  }
+
+  return intersection;
+}
+
+// *** Solution 2 ***
+// function arrayIntersection(arr1, arr2) {
+//   const set1 = new Set(arr1);
+//   const intersection = [];
+
+//   for (let num of arr2) {
+//     if (set1.has(num)) {
+//       intersection.push(num);
+//     }
+//   }
+
+//   return intersection;
+// }
 
 module.exports = arrayIntersection;

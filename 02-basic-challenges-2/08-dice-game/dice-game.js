991e8bb5ee060fd5fea874af6e1ef1bf5e754e44
Emmanuel Martinez <jemes-developer@hotmail.com>
2025-09-22 20:47:32 -0600

    23. Dice Game Simulation

--- 02-basic-challenges-2/08-dice-game/dice-game.js
+++ 02-basic-challenges-2/08-dice-game/dice-game.js
@@ -1,3 +1,31 @@
-function diceGameSimulation() {}
+function diceGameSimulation(numSimulations) {
+  const arr = [];
+
+  const handleResult = (sum) => {
+    if (sum === 7 || sum === 11) {
+      return "win";
+    } else if (sum === 2 || sum === 3 || sum === 12) {
+      return "lose";
+    }
+
+    return "roll again";
+  };
+
+  for (let i = 0; i < numSimulations; i++) {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+    const sum = dice1 + dice2;
+    const result = handleResult(sum);
+
+    arr.push({
+      dice1,
+      dice2,
+      sum,
+      result,
+    });
+  }
+
+  return arr;
+}
 
 module.exports = diceGameSimulation;

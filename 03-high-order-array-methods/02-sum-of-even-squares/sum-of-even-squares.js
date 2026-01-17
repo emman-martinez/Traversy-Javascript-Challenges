86815bad31a2797f5a9200d369781cdf0e05f3e4
Emmanuel Martinez <jemes-developer@hotmail.com>
2026-01-16 21:35:25 -0600

    27. Sum Of Even Squares

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,11 @@
-function sumOfEvenSquares() {}
+function sumOfEvenSquares(numbers) {
+  if (numbers.length === 0) return 0;
+
+  const evenNumbers = numbers.filter((num) => num % 2 === 0);
+  const square = evenNumbers.map((num) => Math.pow(num, 2));
+  const sum = square.reduce((total, num) => total + num);
+
+  return sum;
+}
 
 module.exports = sumOfEvenSquares;
